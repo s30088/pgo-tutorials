47ed2c17401a0252f4251e768e796aa3d3f2beb6
s30088 <s30088@pjwstk.edu.pl>
2024-03-17 14:35:55 +0100

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        zad1();
+    }
+
+    public static void zad1() {
+        System.out.println("");
+    }
+}
